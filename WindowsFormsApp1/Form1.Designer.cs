34d7d1f2be060f1a57ba070f9b3f5aa013f50a4b
spk42492@iencm.com <spk42492@iencm.com>
2021-02-19 23:30:42 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+            this.pictureBox.Location = new System.Drawing.Point(195, -2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button.Location = new System.Drawing.Point(37, 176);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(128, 128);
+            this.button.TabIndex = 1;
+            this.button.Text = "\r\nSolar system";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(59, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Our Planet";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "orbit radius in astronomical units";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "planet radius in kilometers";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(596, 382);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+  
+        public void drawning()
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Gold);
+            float centerX = 200;
+            float radius = 20;
+            float centerY = 200;
+            g.DrawEllipse(pen, centerX - radius, centerY - radius, radius + radius, radius + radius);
+
+        }
+        public void drawningPluto()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5; // каждые n миллисекунд
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.White);
+            float centerX = 200;
+            float radius = 2;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);              
+                centerX2 = centerX2 + 185 * Math.Cos((Math.PI * i* 0.002) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 185 * Math.Sin((Math.PI * i * 0.002) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+               
+            });
+            timer.Start();
+        }
+
+        public void drawningNeptune()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.LightBlue);
+            float centerX = 200;
+            float radius = 7;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 165 * Math.Cos((Math.PI * i * 0.003) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 165 * Math.Sin((Math.PI * i * 0.003) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningJupiter()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Orange);
+            float centerX = 200;
+            float radius = 12;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 90 * Math.Cos((Math.PI * i * 0.042) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 90 * Math.Sin((Math.PI * i * 0.042) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningUranus()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.DarkBlue);
+            float centerX = 200;
+            float radius = 6;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 145 * Math.Cos((Math.PI * i * 0.006) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 145 * Math.Sin((Math.PI * i * 0.006) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningMars()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.DarkRed);
+            float centerX = 200;
+            float radius = 4;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 65 * Math.Cos((Math.PI * i * 0.265) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 65 * Math.Sin((Math.PI * i * 0.265) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningEarth()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Blue);
+            float centerX = 200;
+            float radius = 4;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 50 * Math.Cos((Math.PI * i * 0.5) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 50 * Math.Sin((Math.PI * i * 0.5) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningVenus()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.WhiteSmoke);
+            float centerX = 200;
+            float radius = 3;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 35 * Math.Cos((Math.PI * i * 0.81) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 35 * Math.Sin((Math.PI * i * 0.81) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningMercury()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Gray);
+            float centerX = 200;
+            float radius = 2;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 25 * Math.Cos((Math.PI * i * 2.07) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 25 * Math.Sin((Math.PI * i * 2.07) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        public void drawningSaturn()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Beige);
+            float centerX = 200;
+            float radius = 10;
+            float centerY = 200;
+            double i = 1;
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + 120 * Math.Cos((Math.PI * i * 0.016) / 180);
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - 120 * Math.Sin((Math.PI * i * 0.016) / 180);
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            drawning();
+            drawningPluto();
+            drawningUranus();
+            drawningSaturn();
+            drawningJupiter();
+            drawningMars();
+            drawningEarth();
+            drawningVenus();
+            drawningMercury();
+            drawningNeptune();
+        }    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            Timer timer = new Timer();
+            timer.Interval = 5;
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Brushes.Gold);
+            string RadiusOrbit = textBox2.Text;          
+            float centerX = 200;           
+            string RadiusPlanet = textBox1.Text;             
+            float radius = Convert.ToSingle(RadiusPlanet)/3000;
+            float centerY = 200;
+            double RadiusOrbit2 = Convert.ToDouble(RadiusOrbit)*20;
+            float radius2 = 20;
+            g.DrawEllipse(pen, centerX - radius2, centerY - radius2, radius2 + radius2, radius2 + radius2);
+            pen.Color = colorDialog1.Color;          
+            double i = 1;
+            double R = Convert.ToDouble(RadiusOrbit);
+            timer.Tick += new EventHandler((o, ev) =>
+            {
+                double centerX2 = Convert.ToDouble(centerX);
+                double centerY2 = Convert.ToDouble(centerY);
+                centerX2 = centerX2 + RadiusOrbit2* 7/3 * Math.Cos((Math.PI * i * Math.Pow(R, -4)/350));
+                int centerX3 = Convert.ToInt32(centerX2);
+                centerY2 = centerY2 - RadiusOrbit2* 7/3 * Math.Sin((Math.PI * i * Math.Pow(R, -4) / 350));
+                int centerY3 = Convert.ToInt32(centerY2);
+                g.DrawEllipse(pen, centerX3 - radius, centerY3 - radius, radius + radius, radius + radius);
+                i++;
+
+            });
+            timer.Start();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
